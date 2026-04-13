4141e50cc266b17c867271c260efa4e9c2b4fb53
Tyler B. <tylerthebr@users.noreply.github.com>
2026-04-13 08:46:39 +0800

    add todo note about potential landing page

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import { redirect } from 'next/navigation';
 
 // Redirect to studio page on load
 // Could change this to '/dashboard' or '/home' if we add those routes later
+// TODO: maybe add a landing page here someday with project info
 export default function Home() {
   redirect('/studio');
 }
