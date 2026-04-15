1c506cfdd8e903f07705cc432363143fa27ccb1f
Tyler B. <tylerthebr@users.noreply.github.com>
2026-04-15 22:28:14 +0800

    add note about trying /studio/new as landing target

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import { redirect } from 'next/navigation';
 // Could change this to '/dashboard' or '/home' if we add those routes later
 // TODO: maybe add a landing page here someday with project info
 // NOTE: changed redirect target to '/studio' from default - keeping this as reference
+// UPDATE: trying '/studio/new' to land directly on a new project instead of the list
 export default function Home() {
   // Redirecting to studio for now; swap to '/dashboard' once I build that out
   redirect('/studio');
